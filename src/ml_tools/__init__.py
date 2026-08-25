7aa32cbbaec1e0a2067c7c5078cf742c3738962b
Patrik Müller <patrik.mueller@ost.ch>
2026-08-25 17:27:57 +0200

    Only export public API

--- src/ml_tools/__init__.py
+++ src/ml_tools/__init__.py
@@ -1,7 +1,6 @@
 """Tools for machine learning."""
 
-from ml_tools.mac import analyze, config, detection, fit, metric, plot
-from ml_tools.mac.analyze import ModelAdequacyChecker
+from ml_tools.mac.analyze import MAC, ModelAdequacyChecker
 from ml_tools.mac.config import MACConfig
 from ml_tools.mac.detection import ProblematicSampleMasks
 from ml_tools.mac.fit import FitSummary
@@ -11,15 +10,10 @@ from . import datasets
 
 __all__ = [
     "datasets",
+    "MAC",
     "ModelAdequacyChecker",
     "MACConfig",
     "ProblematicSampleMasks",
     "FitSummary",
     "MetricSummary",
-    "analyze",
-    "config",
-    "detection",
-    "fit",
-    "metric",
-    "plot",
 ]
